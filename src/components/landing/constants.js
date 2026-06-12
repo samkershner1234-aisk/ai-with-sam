b2869b3a569c2dad83b168915afb88b6192a896a
samkershner1234-aisk <samkershner1234@gmail.com>
2026-06-12 11:02:41 +0300

    Create constants.js

---
+++ src/components/landing/constants.js
@@ -0,0 +1,3 @@
+export const SAM_PHOTO = "https://media.base44.com/images/public/6a265985622448036bcd68b3/6b6af48a7_Screenshot_2025-07-04-16-03-43-56_965bbf4d18d205f782c6b8409c5773a4.jpg";
+export const CTA_URL = "https://calendly.com/samtheaicoach/free";
+export const WHATSAPP_URL = "https://wa.me/972526198680";
