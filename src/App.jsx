0ad59c9451ce7e05fdb521bd0375470aa6b7a873
samkershner1234-aisk <samkershner1234@gmail.com>
2026-08-09 10:19:31 +0300

    Add Teams route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import AiTimeWasteAudit from './pages/AiTimeWasteAudit';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AiTimeWasteAudit from "./pages/AiTimeWasteAudit";
+import Teams from "./pages/Teams";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ai-time-waste-audit" element={<AiTimeWasteAudit />} />
+        <Route path="/teams" element={<Teams />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
