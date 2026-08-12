f0962cfe587b57736bb73bd6258b0c1a296d7e5e
samkershner1234-aisk <samkershner1234@gmail.com>
2026-08-12 08:28:37 +0300

    Add ScrollToTop component to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AiTimeWasteAudit from "./pages/AiTimeWasteAudit";
 import Teams from "./pages/Teams";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ai-time-waste-audit" element={<AiTimeWasteAudit />} />
@@ -13,7 +15,7 @@ function App() {
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
-  );
+    );
 }
 
 export default App;
