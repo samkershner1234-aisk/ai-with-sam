0af82e3003dc7576e8c278e069b2716a54d348a9
samkershner1234-aisk <samkershner1234@gmail.com>
2026-08-29 22:53:05 +0300

    Add Privacy route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AiTimeWasteAudit from "./pages/AiTimeWasteAudit";
 import Teams from "./pages/Teams";
+import Privacy from "./pages/Privacy";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
@@ -12,10 +13,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/ai-time-waste-audit" element={<AiTimeWasteAudit />} />
         <Route path="/teams" element={<Teams />} />
+        <Route path="/privacy" element={<Privacy />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
-    );
+  );
 }
 
 export default App;
